594730b475a24f201f6df47dc4bfec3cbfa8a51e
juumelisa <jumelisa09@gmail.com>
2022-01-21 10:36:05 +0700

    js3

---
+++ 3a.js
@@ -0,0 +1,31 @@
+const sisiBalok = [12, 10, 3]
+
+const volumeBalok = (sisi) =>{
+    return new Promise((resolve, reject)=>{
+        let volume = 1
+        setTimeout(()=>{
+            if(sisi.length==3){
+                sisi.forEach(element =>{
+                    if(typeof element=="number" && element>0){
+                        volume *= element
+                    }else{
+                        volume = 0
+                    }
+                })
+            }
+            if(volume!==0){
+                resolve(`Volume Balok = ${volume}`)
+            }else{
+                reject("Data tidak sesuai")
+            }
+        }, 3000)
+    })
+}
+
+volumeBalok(sisiBalok).then((res)=>{
+    console.log(res)
+}).catch((err)=>{
+    console.log(err)
+})
+
+console.log(sisiBalok)
---
+++ 3b.js
@@ -0,0 +1,26 @@
+const waktuMundur = (sisa)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            if(sisa>0){
+                resolve(`${sisa} detik`)
+            }else{
+                resolve("Waktu habis")
+            }
+        }, 1000)
+    })
+}
+
+async function sisaWaktu(times){
+    try{
+        while(times>=0){
+            const results = await waktuMundur(times)
+            console.log(results)
+            times--
+        }
+    }catch(err){
+        console.log(err)
+    }
+}
+
+sisaWaktu(25)
+
